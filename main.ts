414e966eb104255e6616b6a0be7efab0b802816d
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-10-03 10:57:38 -0400

    turns on and off a pin

--- main.ts
+++ main.ts
@@ -5,4 +5,19 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+//setup//
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P16, 0)
+
+//turn on led
+input.onButtonPressed(Button.A, function() { 
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+//turn off led
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
